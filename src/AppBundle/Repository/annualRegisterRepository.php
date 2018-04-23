5058c8ad7808251559c2e353bcf784ae9fbfdaf0
leoben3 <rlezamasanchez@gmail.com>
2018-04-23 12:58:19 +0200

    "añadido metodo a repositorio de registrosAnuales"

--- src/AppBundle/Repository/annualRegisterRepository.php
+++ src/AppBundle/Repository/annualRegisterRepository.php
@@ -8,9 +8,32 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\annualRegister;
 use Doctrine\ORM\EntityRepository;
 
 class annualRegisterRepository extends EntityRepository
 {
+
+    public function findAllByPollutantAndWeatherStation($idPollutant,$weatherStation)
+    {
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+
+        $qb->select('p')
+            ->from(annualRegister::class, 'p')
+            ->where('p.pollutant = :idPollutant')
+            ->andWhere('p.$weatherStation = :idWeatherStation')
+            ->setParameters(array(
+
+                'idPollutant' => $idPollutant,
+                'idWeatherStation' => $weatherStation
+            ))
+            ->orderBy('p.year','DESC');
+
+        $query = $qb->getQuery();
+        return $query->getResult();
+    }
+
+
 
 }
