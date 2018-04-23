9b4dcb01b0dbef9feb15cdb62472a503f94e7c83
leoben3 <rlezamasanchez@gmail.com>
2018-04-23 15:18:48 +0200

    "añadido FOSRestBundle"

---
+++ src/AppBundle/Controller/pollutantController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruben
+ * Date: 23/04/18
+ * Time: 14:52
+ */
+
+namespace AppBundle\Controller;
+
+
+class pollutantController
+{
+
+}
--- src/AppBundle/Entity/pollutant.php
+++ src/AppBundle/Entity/pollutant.php
@@ -10,6 +10,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\pollutantRepository")
