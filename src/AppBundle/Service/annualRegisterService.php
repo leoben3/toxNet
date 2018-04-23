5f7cb76201a5a12b6e56fbd3caf38da8ce941023
leoben3 <rlezamasanchez@gmail.com>
2018-04-23 14:37:43 +0200

    "añadidos servicios recuperación repositorios"

---
+++ src/AppBundle/Service/annualRegisterService.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruben
+ * Date: 23/04/18
+ * Time: 13:25
+ */
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\annualRegister;
+
+class annualRegisterService
+{
+    protected $modelClass = annualRegister::class;
+
+    public function getAllByPollutantAndWeatherStation($doctrine,$idPollutant,$weatherStation)
+    {
+        return $doctrine->getRepository($this->modelClass)->findAllByPollutantAndWeatherStation($idPollutant,$weatherStation);
+    }
+}
---
+++ src/AppBundle/Service/pollutantService.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruben
+ * Date: 23/04/18
+ * Time: 13:13
+ */
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\pollutant;
+
+class pollutantService
+{
+
+    protected $modelClass = pollutant::class;
+
+    public function findOneByName($doctrine, $name)
+    {
+        $repository = $doctrine->getRepository($this->modelClass);
+
+        return $repository->findOneBy(['name' => $name]);
+    }
+
+}
