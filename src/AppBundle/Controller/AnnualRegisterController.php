96f903298777bbddca810d57156c175500196985
leoben3 <rlezamasanchez@gmail.com>
2018-05-05 13:18:49 +0200

    modify problem with routing

--- src/AppBundle/Controller/AnnualRegisterController.php
+++ src/AppBundle/Controller/AnnualRegisterController.php
@@ -9,19 +9,12 @@ class AnnualRegisterController extends DefaultController
 
     public function getAnnualRegisters()
     {
-        $url = $this->generateUrl(
-            'registers'
-        );
-
 
     }
 
 
     public function getAnnualRegister()
     {
-        $url = $this->generateUrl(
-            'register_item'
-        );
 
     }
 
--- src/AppBundle/Controller/PollutantController.php
+++ src/AppBundle/Controller/PollutantController.php
@@ -10,20 +10,12 @@ class PollutantController extends DefaultController
 
     public function getPollutants()
     {
-        $url = $this->generateUrl(
-            'pollutants'
-        );
-
 
     }
 
 
     public function getPollutant()
     {
-        $url = $this->generateUrl(
-            'pollutant_item'
-        );
-
 
     }
 
