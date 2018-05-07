f90ddaf41172ba668b23c687fba7dd74ac97ecda
leoben3 <rlezamasanchez@gmail.com>
2018-05-07 16:14:31 +0200

    added tests

---
+++ tests/AppBundle/Controller/AnnualRegisterControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class AnnualRegisterControllerTest extends KernelTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+
+}
---
+++ tests/AppBundle/Service/AnnualRegisterServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use FOS\RestBundle\Tests\Functional\WebTestCase;
+
+class AnnualRegisterServiceTest extends WebTestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+}
